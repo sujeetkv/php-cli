40cb22ca2a2aeaaf1f60a6f5ddf65ca416a3e4e1
Sujeet Kumar <sujeetkv90@gmail.com>
2017-02-19 01:55:03 +0530

    updated doc block

--- src/Cli.php
+++ src/Cli.php
@@ -2,7 +2,8 @@
 namespace SujeetKumar\PhpCli;
 
 /**
- * Cli utility class for PHP Command Line Interface
+ * PHP library to build command line tools
+ *
  * @author  Sujeet <sujeetkv90@gmail.com>
  * @link    https://github.com/sujeet-kumar/php-cli
  */
